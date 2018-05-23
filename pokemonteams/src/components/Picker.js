e1ab721648e17d6939e36c3873da90df966f239c
Demetrius Berkeley <dberkeley@Demetriuss-MacBook-Pro-2.local>
2018-05-23 11:11:22 -0400

    structing file for database login

---
+++ pokemonteams/src/components/Picker.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Picker extends React.Component {
+    render(){
+        return(
+            <h1>Picking...</h1>
+        )
+    }
+}
+
+export default Picker
---
+++ pokemonteams/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from "react-dom";
+import Picker from './components/Picker';
+
+render(<Picker />, document.querySelector('#main'))
